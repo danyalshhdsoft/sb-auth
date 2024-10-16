499d4325d269e8cf7e4de21311620cc59d93b9ca
Shaheer-Moidin-SB <moidinsb24@gmail.com>
2024-10-16 11:25:38 +0400

    added clientId:'property' in main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
       transport: Transport.KAFKA,
       options: {
         client: {
+          clientId: 'property',
           //brokers: ['host.docker.internal:9092'],
           brokers: ['localhost:9092'],
         },
